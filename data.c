2d24c24fea28b5dbb2f824ce85713b87f3416cab
Iad ABDUL RAOUF <iad.abdulraouf@cea.fr>
2023-03-13 02:57:33 +0100

    rewrite data.h and write data.c

---
+++ data.c
@@ -0,0 +1,46 @@
+#include "data.h"
+
+#include <stdlib.h>
+#include <stdio.h>
+
+struct Data{
+    // dimension of each individual sensor readings
+    unsigned int dim;
+    // length : number of element
+    unsigned int len;
+    // values
+    double* vals;
+};
+
+Data* createData(unsigned int dim, unsigned int len){
+    Data* data = malloc(sizeof(Data));
+    if (data==NULL){
+        printf("function createData failed to allocate data");
+        exit(EXIT_FAILURE);
+    }
+    data->dim = dim;
+    data->dim = len;
+    data->vals = malloc(dim*len*sizeof(double));
+    if (data->vals==NULL){
+        printf("function createData failed to allocate data->vals");
+        exit(EXIT_FAILURE);
+    }
+    return data;
+}
+
+void destroyData(Data* data){
+    free(data->vals);
+    free(data);
+}
+
+double* getVal(Data* data, unsigned int i){
+    return &data->vals[i*data->dim];
+}
+
+unsigned int getLen(Data* data){
+    return data->len;
+}
+
+unsigned int getDim(Data* data){
+    return data->dim;
+}
--- data.h
+++ data.h
@@ -1,31 +1,28 @@
 #ifndef DATA_H
 #define DATA_H
 
-// TODO ne mettre que la declaration sans le détail ici pour protéger les champs.
-// declare the struct Data without it's member to protect them from modification 
-// by users including data.h.
+// Data is a convenient structure to store and access N vectors of dimention D.
 
-typedef struct Data{
-    // dimension of each individual sensor readings
-    unsigned int dim;
-    // length : number of element
-    unsigned int len;
-    // values
-    double** vals;
-} Data;
+// Notice that the struct Data is declared without it's member. the goal is to 
+// protect them. Users including data.h cannot mess up with Data. They can only
+// interact with it through the exposed functions.
+// Remarq : if you have an "incomplete type" bug it is because the user can only
+// handle Data pointers and not Data itself
+// Data data = ...; // is not allowed "incomplete type"
+// Data* data = ...; // is allowed
+// This design pattern is called a FIRST-CLASS ADT pattern.
+// http://adampetersen.se/Patterns%20in%20C%201.pdf
 
-Data createData(unsigned int dim, unsigned int len, double** vals);
-Data destroyData(Data data);
+typedef struct Data Data;
 
-double* getVal(Data data, unsigned int i);
-unsigned int getLen(Data data);
-unsigned int getDim(Data data);
+// "dim" is the dimention of each vectors
+// "len" is the number of vector to store
+Data* createData(unsigned int dim, unsigned int len);
+void destroyData(Data* data);
 
-// TODO depalcer
-// A good practice might be to separate read and write csv from the above
-// code and put it in another file. Essentially Data is just a container and it
-// could be created by other means. It is not specific to csv file format.
-Data readCsv(char* csvName);
-void writeCsv(char* csvName, Data data);
+// getVal return the reference of the first element of the i-th vector
+double* getVal(Data* data, unsigned int i);
+unsigned int getLen(Data* data);
+unsigned int getDim(Data* data);
 
 #endif
