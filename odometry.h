2cd930f5107210b417f128c24c39977de4a99cac
Iad ABDUL RAOUF <iad.abdulraouf@cea.fr>
2023-03-19 01:54:51 +0100

    simplify odometry.h

--- odometry.h
+++ odometry.h
@@ -3,13 +3,10 @@
 #ifndef ODOMETRY_H
 #define ODOMETRY_H
 
-typedef Data Odoms;
-typedef double* Odom;
-
 // getters
 // (u is a standard notation for odometry input)
-double OdomGetUx(Odom u);
-double OdomGetUy(Odom u);
-double OdomGetUpsi(Odom u);
+double odomGetUx(double* odom);
+double odomGetUy(double* odom);
+double odomGetUpsi(double* odom);
 
 #endif
