d39594dda45a37e29cdb91409a878a63711858cf
Iad ABDUL RAOUF <iad.abdulraouf@cea.fr>
2023-03-19 01:51:32 +0100

    update moveParticles interface with a void* parameter

--- moveParticlesStrategy.h
+++ moveParticlesStrategy.h
@@ -4,12 +4,14 @@
 #define MOVEPARTICLESSTRATEGY_H
 
 // MoveParticlesStrategy define the interface of an moveParticles
-// function. It allows you to switch between different implementation
-// in main.c without any code modification (in particular, without
-// dirty copying and pasting and/or commenting out blocks of code...)
+// function. It allows you easily to switch between different
+// implementation in main.c 
 
-// see the commentary inside "resamplingStrategy.h" for
-// a detailed explanation
-typedef void (*MoveParticlesStrategy)(double* logweight, Data* states);
+// Similar to "resamplingStrategy.h" but it also include a void*
+// parameter. Because various implementation will each have their own parameter set,
+// void* will allow you to use any paramter type.
+// Be carefull, using void* is unsafe because the compiler has no information on
+// the input type and cannnot detect bugs for you.
+typedef void (*MoveParticlesStrategy)(double* logweight, Data* states, void* params);
 
 #endif
--- moveParticlesXYPsi.h
+++ moveParticlesXYPsi.h
@@ -4,15 +4,32 @@
 #ifndef MOVEPARTICLESXYPSI_H
 #define MOVEPARTICLESXYPSI_H
 
-extern 
+extern double* odom;
+extern double* obs;
+extern MagneticMap* magmap;
+
+typedef struct MoveXYPsiParam{
+    // standard deviation of the odometry magnetometer measurments error
+    // modeled as a gaussian. For simplicity, errors on mx,my,mz are assumed
+    // to be independant and to have the same standard deviation (iid)
+    double std_obs;
+    // standard deviation of the odometry random perturbation 
+    double std_ux;
+    double std_uy;
+    double std_upsi;
+    // magnetometer bias are assumed to be knonw here
+    double bx;
+    double by;
+    double bz;
+}MoveXYPsiParam;
 
 // update all particles
-void moveParticlesXYPsi(double* logweight, StatesXYPsi* states);
+void moveParticlesXYPsi(double* logweight, StatesXYPsi* states, void* params);
 
 // update just one particle
-void moveParticleXYPsi(double* logweight, StateXYPsi state);
+void moveParticleXYPsi(double* logweight, StateXYPsi state, MoveXYPsiParam* params);
 
 // compute lodweight increment
-double logLikelihoodXYPsi(StateXYPsi state, MagneticMap* magmap);
+double logLikelihoodXYPsi(StateXYPsi state, MoveXYPsiParam* params);
 
 #endif
