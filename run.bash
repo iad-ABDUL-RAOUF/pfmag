4a0e000432fb4e76ab80ef064b6f1f7d7e68a7e2
Iad ABDUL RAOUF <iad.abdulraouf@cea.fr>
2023-03-28 03:07:51 +0200

    update paramter to use to start the project

--- run.bash
+++ run.bash
@@ -5,9 +5,9 @@
 # number of particles
 nParticles="10000"
 # odometry file name
-odometryFilename="/home/ia266894/Data/20230300-0_in104/20230326-11_atrium/trajectories/6_2994_3425/odometry.csv"
+odometryFilename="/home/ia266894/Data/20230300-0_in104/20230326-11_atrium/trajectories/1_588_1059/odometry.csv"
 # magnetic observation file name
-observationFilename="/home/ia266894/Data/20230300-0_in104/20230326-11_atrium/trajectories/6_2994_3425/observation.csv"
+observationFilename="/home/ia266894/Data/20230300-0_in104/20230326-11_atrium/trajectories/1_588_1059/observation.csv"
 # magnetic map file name
 mapFilename="/home/ia266894/Data/20230300-0_in104/20230326-11_atrium/magmap.mm"
 # state name : "XYPsi" for (x,y,psi) states. Option 2 makes you implement 'XYPsiBias' too.
@@ -33,7 +33,7 @@ $pfmag $nParticles $odometryFilename $observationFilename $mapFilename $stateNam
 # estimation computed by the particle filter
 estimatesFilename=$outputDirname"estimates.csv"
 # file name of the true trajectory
-groundtruthFilename="/home/ia266894/Data/20230300-0_in104/20230326-11_atrium/trajectories/6_2994_3425/trajectory.csv"
+groundtruthFilename="/home/ia266894/Data/20230300-0_in104/20230326-11_atrium/trajectories/1_588_1059/trajectory.csv"
 # the executable file
 evaluatePfmag="/home/ia266894/Documents/teaching/20230300_ENSTA_IN104/pfmag/evaluatePfmag"
 
