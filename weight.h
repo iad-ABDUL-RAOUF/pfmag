a9ccd953d450eb4070cf1c2aca96d83d263b91e4
Iad ABDUL RAOUF <iad.abdulraouf@cea.fr>
2023-03-28 05:56:49 +0200

    document weight.h

--- weight.h
+++ weight.h
@@ -1,7 +1,8 @@
 #include "data.h"
 
+// allocate Data of size len x 1
 Data* createLogweights(unsigned int len);
-
+// allocate Data of size len x 1 and fill it with normilized weight computed from logweights
 Data* createNormalisedWeights(const Data* logweights);
-
+// return log(sum(weights)) from logweight data. Usefull to compute normalised weights
 double logSumWeight(const Data* logweights);
